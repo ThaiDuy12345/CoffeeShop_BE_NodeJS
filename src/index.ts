09cd05ceea57e86ca7738bb3998ac8cdb2d93048
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-10-11 01:32:15 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,11 @@ app.use(cors({
   origin: "http://localhost:4200",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
+  allowedHeaders: [
+    'Access-Control-Allow-Origin: http://localhost:4200',
+    'Access-Control-Allow-Methods: GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Credentials: true'
+  ]
 }));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
